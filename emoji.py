a06d810276cb8aca30618d72f201f63dab949a03
Anubhav Prasad <116714987+anu-0035@users.noreply.github.com>
2022-12-08 22:29:17 +0530

    Update emoji.py

--- emoji.py
+++ emoji.py
@@ -1,4 +1,3 @@
 import emoji
-
-print(emoji.emojize("I love reading books:books:"))
-print(emoji.emojize("Some people have a very sensitive heart:red_heart:, please be kind with them.:hibiscus:"))
+print(emoji.emojize("Time to go to the beast mode."))
+print(emoji.emojize("its me_and_only_me"))
